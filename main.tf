24eebb9609886600f58c53c0734ca6a7120b63a6
junyaiwasaki-git <junya.iwasaki@rakus-partners.co.jp>
2025-10-14 14:25:13 +0900

    Terraform destroy4

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket = "iwasakij-tfstate-bucket" # ← あなたのS3バケット名
+    key    = "terraform.tfstate"
+    region = "us-west-2"
+  }
+}
+
 provider "aws" {
   region = "us-west-2"
 }
